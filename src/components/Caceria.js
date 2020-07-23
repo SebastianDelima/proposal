0afe62ddf41011478660f3e9a8ffc3fe5713b32a
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-07-23 10:59:55 -0500

    caceria skeleton

--- src/components/Caceria.js
+++ src/components/Caceria.js
@@ -1,11 +1,26 @@
 import React, { Component, Fragment } from 'react';
-
+import { NavLink } from 'react-router-dom';
 
 export default class Caceria extends Component {
     render(){
         return(
             <Fragment>
-                <div>caceria</div>
+                <container class='nav-bar'>
+                    <img src='../public/favico.ico' alt='logo'></img>
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/about'>About</NavLink>
+                    <NavLink to='/camps'>Camps</NavLink>
+                </container>
+                <container id="img-intro-cont">
+                    <div id="img-intro">en Sirius nos importa salud bienestar, en Sirius nos importa salud bienestar, en Sirius nos importa salud bienestar
+                    en Sirius nos importa salud bienestaren Sirius nos importa salud bienestaren Sirius nos importa salud bienestaren Sirius nos importa salud bienestar
+                    en Sirius nos importa salud bienestaren Sirius nos importa salud bienestaren Sirius nos importa salud bienestaren Sirius nos importa salud bienestar
+                    en Sirius nos importa salud bienestaren Sirius nos importa salud bienestaren Sirius nos importa salud bienestaren Sirius nos importa salud bienestar</div>
+                    <p id="safaris">Safaris</p>
+                </container>
+                <container>
+                    
+                </container>
             </Fragment>
             
         )
